76ea9a55987868b0dcc31dc7b7709cd591a55acf
Rohit Bhojak <rohitbhojak99@gmail.com>
2026-02-19 23:11:47 +0530

    Save cart to localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { Outlet } from "react-router";
 import Navbar from "./pages/Navbar";
-import { useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import useTheme from "./hooks/useTheme";
 import cartReducer from "./reducers/cartReducer";
 
 export default function App() {
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
-  const [cart, dispatch] = useReducer(cartReducer, JSON.parse(localStorage.getItem("cart")) || {});
+  const [theme, setTheme] = useState(() => localStorage.getItem("theme") || "light");
+  const [cart, dispatch] = useReducer(
+    cartReducer,
+    {},
+    () => JSON.parse(localStorage.getItem("cart")) || {},
+  );
 
   useTheme(theme);
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   const toggleTheme = () => {
     setTheme(theme === "light" ? "dark" : "light");
